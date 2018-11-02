33aebd11ab0a0afdb815abb5b17c14363c59b921
Alvaro <44395277+fernandezcarballo@users.noreply.github.com>
2018-11-02 15:07:44 +0100

    Update lenguaje1.pl

--- lenguaje1.pl
+++ lenguaje1.pl
@@ -1,3 +1,4 @@
+
 #!/usr/bin/perl
 print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
@@ -21,24 +22,26 @@ if($ENV{'QUERY_STRING'} ne "") {
 	 @arGet=split("=",$ENV{'QUERY_STRING'});
    if($arGet[0] eq 'lenguaje') {
 	     print "El lenguaje escogido es: $arGet[1]";
-       print "<h2> Porque te gusta mas el lenguaje de programacion: $arGet[1]</h2>
+       print "<h2> Porrrrrque te gusta mas el lenguaje de programacion: $arGet[1]</h2>
           <form name=\"search\" method=\"get\" >";
        open F, "/home/alvarofe/Escritorio/respuestas.txt";
        while(<F>) {
          chomp;
-         @campos=split(",");
-         $ar{$campos[0]}=$campos[0];
-   }
-   print "<select name = \"respuesta\">";
-   for (keys %ar){
-     print "<option value=$_";
-     print ">$_</option>";
-   }  
- print "</select>
-         <input type=\"submit\" value=\"Enviar\" /> \n\n";
-}
+         #@campos=split(",");
+         #$ar{$campos[0]}=$campos[0];
+         push @ar,$_;
+       }
+       #push @ar,"muy%20interesante";
+       print "<select name = \"respuesta\">";
+       for (@ar){
+          print "<option value=$_";
+          print ">$_</option>";
+       }  
+       print "</select>
+         <input type=\"submit\" value=\"Enviar\" />";
+  }
   if($arGet[0] eq 'respuesta') {
-  @ariGet=split("=",$ENV{'QUERY_STRING'});
-  print "te gusta porque: $ariGet[1]";
-}
+    #@ariGet=split("=",$ENV{'QUERY_STRING'});
+    print "te gusta porque: $arGet[1]";
+  }
 }
